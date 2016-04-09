81b4985583e566fc2c56337b2e5490d90068e4d2
campustrampus <lapierreni@gmail.com>
2016-04-09 04:25:52 +0000

    Add post object migrations and some view changes.

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  content = models.TextField()
+  pub_date = models.DateTimeField('date published', auto_now_add=True)
+ 
+  def __str__(self):
+    return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 def index(request):
-  return render(request,'blog/index.html', {})
+  blog_posts = Post.objects.order_by('-pub_date')[:10]
+  context = {'blog_posts':blog_posts}
+  return render(request,'blog/index.html', context)
 
 def post_list(request):
   return render(request, 'blog/post_list.html', {})
